d084c32cc0f7c94d3d0431bfb26e870cbbf8b014
Celso Aquino <celso.aquino@ymail.com>
2022-05-30 22:34:15 -0300

    created Profile

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import Layout from "./components/layout";
+import Profile from "./components/profile";
 
 function App() {
-  return (
-    <main>
-        <Layout>
-            <div>
-                <img source="https://avatars.githubusercontent.com/u/21125748?v=4" alt="avatar User"/>
-            <h1>Celso Aquino</h1>
-                <h3>Username</h3>
-                <span>celsoaquino</span>
-                <div>
-                    <div>
-                        <h4>Followers</h4>
-                        <span>5</span>
-                    </div>
-                    <div>
-                        <h4>Followers</h4>
-                        <span>5</span>
-                    </div>
-                    <div>
-                        <h4>Followers</h4>
-                        <span>5</span>
-                    </div>
-                    <div>
-                        <h4>Followers</h4>
-                        <span>5</span>
-                    </div>
+    return (
+        <main>
+            <Layout>
+                <Profile />
 
-                </div>
-            </div>
-            <div>Repositories</div>
-            <div>Starreds</div>
-        </Layout>
-    </main>
-  );
+                <div>Repositories</div>
+                <div>Starreds</div>
+            </Layout>
+        </main>
+    );
 }
 
 export default App;
---
+++ src/components/profile/index.js
@@ -0,0 +1,45 @@
+import * as S from './styled'
+
+function Profile() {
+
+    return (
+        <S.Wrapper>
+
+            <S.WrapperImage src="https://avatars.githubusercontent.com/u/21125748?v=4" alt="avatar User"/>
+            <S.WrapperInfoUser>
+                <div>
+                    <h1>Celso Aquino</h1>
+                    <S.WrapperUserName>
+                        <h3>Username:</h3>
+                        <a href="https://github.com/celsoaquino"
+                           target="_blank"
+                           rel="noreferrer">
+                            <span>celsoaquino</span>
+                        </a>
+
+                    </S.WrapperUserName>
+                </div>
+                <S.WrapperStatusCount>
+                    <div>
+                        <h4>Followers</h4>
+                        <span>5</span>
+                    </div>
+                    <div>
+                        <h4>Followers</h4>
+                        <span>5</span>
+                    </div>
+                    <div>
+                        <h4>Followers</h4>
+                        <span>5</span>
+                    </div>
+                    <div>
+                        <h4>Followers</h4>
+                        <span>5</span>
+                    </div>
+                </S.WrapperStatusCount>
+            </S.WrapperInfoUser>
+        </S.Wrapper>
+    )
+}
+
+export default Profile
